34fc74c5f463c5d4b5c9e5ad2c3a9878551cc1f4
Leyenda Original <jorge.12528@yahoo.com>
2020-04-09 22:31:06 -0600

    Âupdate

--- huerto.py
+++ huerto.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify, request, json
+from flask_mysqldb import MySQL
+from datetime import datetime
+from flask_cors import CORS
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt)
 
 app = Flask(__name__)
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'huerto'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+app.config['JWT_SECRET_KEY'] = 'secret'
 
-@app.route("/")
+mysql = MySQL(app)
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
+
+CORS(app)
+
+@app.route("/login")
 def home():
-    return "hola python"
+    #code login, for the moment all right
+    return "Hola con Python"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)
